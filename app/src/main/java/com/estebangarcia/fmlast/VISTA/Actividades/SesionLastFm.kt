f629585170f0b6fff1b84f870e97c53bae5b4bbc
joseegarcia000000 <61840664+joseegarcia000000@users.noreply.github.com>
2020-08-15 09:59:23 -0500

    Actividad para mostrar datos GEO al usuario.

---
+++ app/src/main/java/com/estebangarcia/fmlast/VISTA/Actividades/SesionLastFm.kt
@@ -0,0 +1,13 @@
+package com.estebangarcia.fmlast.VISTA.Actividades
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.estebangarcia.fmlast.R
+
+class SesionLastFm : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sesion_last_fm)
+    }
+}
