a8782ed3555b73dcf3ec63585ad6be6777e8b45e
joseegarcia000000 <61840664+joseegarcia000000@users.noreply.github.com>
2020-08-15 19:12:31 -0500

    Actividad para iniciar sesion

---
+++ app/src/main/java/com/estebangarcia/fmlast/VISTA/Actividades/IniciarSesion.kt
@@ -0,0 +1,13 @@
+package com.estebangarcia.fmlast.VISTA.Actividades
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.estebangarcia.fmlast.R
+
+class IniciarSesion : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_iniciar_sesion)
+    }
+}
