b05d3865b944e6bc30b3ab6ab681b8fa75539aec
joseegarcia000000 <61840664+joseegarcia000000@users.noreply.github.com>
2020-08-16 13:20:10 -0500

    Se crea una instancia de databinding del layout Se crea las acciones para los botones anterior/siguiente de paginación

--- app/src/main/java/com/estebangarcia/fmlast/VISTA/Fragmentos/CancionesFragment.kt
+++ app/src/main/java/com/estebangarcia/fmlast/VISTA/Fragmentos/CancionesFragment.kt
@@ -5,20 +5,52 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 
 import com.estebangarcia.fmlast.R
+import com.estebangarcia.fmlast.databinding.FragmentCancionesBinding
+import kotlinx.android.synthetic.main.fragment_canciones.*
 
-/**
- * A simple [Fragment] subclass.
- */
 class CancionesFragment : Fragment() {
+
+    private var LimSup:Int=50
+    private var LimInf:Int=0
+    private var sizelista:Int=34520
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_canciones, container, false)
+
+        val binding=DataBindingUtil.inflate<FragmentCancionesBinding>(inflater,R.layout.fragment_canciones,container,false)
+        (activity as AppCompatActivity)
+
+        binding.textoPaginaInicio.text=LimInf.toString()
+        binding.textoPaginacionActual.text=LimSup.toString()
+        binding.textoPaginacionFinal.text=sizelista.toString()
+
+        binding.botonSiguiente.setOnClickListener{
+            if(sizelista>LimSup){
+                LimSup=LimSup+50
+                LimInf=LimInf+50
+            }
+            actualizarPaginacion()
+        }
+        binding.botonAnterior.setOnClickListener {
+            if(LimInf>=0){
+                LimSup=LimSup-50
+                LimInf=LimInf-50
+            }
+            actualizarPaginacion()
+        }
+
+        return binding.root
     }
 
+    private fun actualizarPaginacion(){
+        textoPaginaInicio.text=LimInf.toString()
+        textoPaginacionActual.text=LimSup.toString()
+        textoPaginacionFinal.text=sizelista.toString()
+    }
 }
