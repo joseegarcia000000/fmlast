3cfb19e2aac88e244826ce826e836d221be046ea
joseegarcia000000 <61840664+joseegarcia000000@users.noreply.github.com>
2020-08-16 13:13:40 -0500

    Se crea una clase base con extension de AppCompactActivity para definir la ejecucion de corrutinas en base a los ciclos de vida de una actividad

---
+++ app/src/main/java/com/estebangarcia/fmlast/VISTA/Base/alcanceActivity.kt
@@ -0,0 +1,31 @@
+package com.estebangarcia.fmlast.VISTA.Base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlin.coroutines.CoroutineContext
+
+abstract class alcanceActivity:AppCompatActivity(),CoroutineScope {
+
+    private lateinit var job: Job
+
+    override val coroutineContext: CoroutineContext
+
+        get() = job + Dispatchers.Main
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        job= Job()
+
+    }
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        job= Job()
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
+    }
+}
